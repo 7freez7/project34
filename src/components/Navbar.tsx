412893e3b2fcd7d520ca3c64d1a252b28a169e2b
7freez7 <filaa.07cz@gmail.com>
2024-11-25 16:17:13 +0100

    main

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,8 +13,15 @@ const Navbar = () => {
       </div>
       <div className="currentLink">
         <Link to="/uvod">ÚVOD</Link>
-        <Link to="/about">O NÁS </Link>
+        <Link to="/about">O ŠKOLE</Link>
         <Link to="/obory">OBORY</Link>
+        <Link to="/obory">Galerie</Link>
+        <Link to="/obory">Dokumenty</Link>
+        <Link to="/obory">CHCI NA ZUŠ</Link>
+
+
+
+        
         <Link to="/kontakt">KONTAKT</Link>
       </div>
     </div>
