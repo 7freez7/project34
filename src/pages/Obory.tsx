3c171527f4d55f35e47749550179f6b1f0f19bc4
7freez7 <filaa.07cz@gmail.com>
2024-11-25 19:41:36 +0100

    origin/gh-pages

--- src/pages/Obory.tsx
+++ src/pages/Obory.tsx
@@ -1,10 +1,39 @@
 import React from "react";
+import { Link } from 'react-router-dom'; // Import Link pro navigaci
+import Art from "../../public/img/Art.jpg";
+import Dance from "../../public/img/Dance.jpg";
+import Hudba from "../../public/img/Hudba.jpg";
 
 const Obory = () => {
   return (
     <div>
       <h1>Obory</h1>
-      <p>nevimmm</p>
+
+      <div className="obor-druh">
+        <Link to="/obory/hudebni"> {""}
+          <h2>Hudební obor</h2>
+          <img src={`${process.env.PUBLIC_URL}/img/Hudba.jpg`} alt="Hudba" className="img" />
+        </Link>
+      </div>
+
+      <div className="obor-druh">
+      <Link to="/obory/tanecni"> {""}
+
+        <h2>Taneční Obor</h2>
+        <img src={`${process.env.PUBLIC_URL}/img/Dance.jpg`} alt="Dance" className="img" />
+        </Link>
+
+      </div>
+
+      <div className="obor-druh">
+      <Link to="/obory/vytvarni"> {""}
+
+        <h2>Výtvarný Obor</h2>
+        <img src={`${process.env.PUBLIC_URL}/img/Art.jpg`} alt="Art" className="img" />
+        </Link>
+
+      </div>
+
     </div>
   );
 };
